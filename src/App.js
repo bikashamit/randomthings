3e35c309592d3f84578d92f8839d28784643f873
amit kumar <amit.gangaur@gmail.com>
2023-08-19 12:00:06 +0530

    web development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [selectedContacts, setSelectedContacts] = useState([]);
+  const [isMobileMessageVisible, setIsMobileMessageVisible] = useState(false);
+
+  const requestContactsPermission = async () => {
+    if (isMobileBrowser()) {
+      try {
+        const contacts = await navigator.contacts.select(['name', 'email']);
+        setSelectedContacts(contacts);
+      } catch (error) {
+        console.error('Error fetching contacts:', error);
+      }
+    } else {
+      setIsMobileMessageVisible(true);
+    }
+  };
+
+  const isMobileBrowser = () => {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phone Book App</h1>
+      <button onClick={requestContactsPermission}>Fetch Contacts</button>
+      {isMobileMessageVisible && (
+        <p>Please open this app in a mobile browser to access contacts.</p>
+      )}
+      <ul>
+        {selectedContacts.map((contact, index) => (
+          <li key={index}>
+            <strong>Name:</strong> {contact.name}{' '}
+            {contact.email && <span>(Email: {contact.email})</span>}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
